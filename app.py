4037ebdd049452fac2ffd920ed00ae18530d7c99
maplecodep <128430550+maplecodep@users.noreply.github.com>
2024-01-09 19:52:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 # Set the AWS API endpoint URL as an environment variable
 api_endpoint = os.getenv("AWS_API_ENDPOINT")
 
-if not api_gateway_endpoint:
+if not api_endpoint:
     st.error("AWS_API_ENDPOINT environment variable not set.")
     st.stop()
 
